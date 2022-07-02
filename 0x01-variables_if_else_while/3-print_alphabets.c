15d17fbfd8397c9a24b038df48751981128d40c7
Emarubu <emarubu945@gmail.com>
2022-07-02 09:52:40 +0000

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Prints the alphabets
+* Returns : 0 is success
+*/
+int main(void)
+
+{
+	char a = a;
+	char Z = Z;
+
+	while (a => Z)
+{
+	putchar 'a';
+	a++;
+}
+	putchar('\n');
+	return (0);
+
+}
+
