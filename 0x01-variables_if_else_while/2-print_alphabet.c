bc74128f669df66706fd0b9d9d0ec8664ba2663e
Emarubu <emarubu945@gmail.com>
2022-07-02 05:45:06 +0000

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabets
+*
+* Return: 0 is success
+*/
+int main(void)
+
+{
+	char a = 'a';
+	char z = 'z';
+
+	while (a <= z)
+{
+	putchar(a);
+	a++;
+}
+	putchar('/n');
+	return (0);
