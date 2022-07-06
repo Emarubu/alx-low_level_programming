9b1777c0aff35225e6bcd3a7baf7d9cbb72a4f64
Emarubu <emarubu945@gmail.com>
2022-07-06 19:38:51 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,12 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 		{
-	 	for (y = 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			product = x * y;
-	 		if (y != 0)
-	 		{
-	  			_putchar(',');
+			if (y != 0)
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
 			if (y == 0)
