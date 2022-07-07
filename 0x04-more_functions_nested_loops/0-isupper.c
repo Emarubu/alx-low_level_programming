626a2c86a7174ef66b4124e763ffa1586423a346
Emarubu <emarubu945@gmail.com>
2022-07-07 03:46:10 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,19 +3,12 @@
 
 
 /**
-
-  * _isupper - Check if a letter is upper
-
-  * @c: The number to be checked
-
-  *
-
-  * Return: 1 for upper letter or 0 for any else
-
-  */
-
+* _isupper - Check if a letter is upper
+* @c: The number to be checked
+*
+* Return: 1 for upper letter or 0 for any else
+*/
 int _isupper(int c)
-
 {
 
 	if (c >= 65 && c <= 90)
