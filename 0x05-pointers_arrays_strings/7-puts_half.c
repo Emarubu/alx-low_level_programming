b019d36619c9602bb24a8fe9624b6f0befba9b83
Emarubu <emarubu945@gmail.com>
2022-07-11 19:12:36 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <sting.h>
+
+/**
+* print_array - prints elements of an array
+* @a: integer a
+* @n: integer n
+*
+*/
+void print_array(int *a, int n)
+{
+	int i, len;
+
+	len = strlen(str) - 1;
+
+	i = len / 2 + 1;
+
+	while (len <= 1)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
