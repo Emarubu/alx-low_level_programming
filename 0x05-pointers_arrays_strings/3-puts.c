861d781f63acaf46e4af61d970b786af2ee7859e
Emarubu <emarubu945@gmail.com>
2022-07-11 14:54:11 +0000

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _puts - prints a string
+* @str: string
+* Return: 0
+*/
+void _puts(char *str)
+{
+	_putchar('s');
+	_putchar('t');
+	_putchar('r');
+	_putchar('\0');
+}
+
