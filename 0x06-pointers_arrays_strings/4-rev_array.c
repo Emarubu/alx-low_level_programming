51498c065d04b95ee4d4e8b0705044d1403ef1da
Emarubu <emarubu945@gmail.com>
2022-07-14 15:18:44 +0000

    I am a kind of paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,12 @@
 */
 void reverse_array(int *a, int n)
 {
-	int aux[n];
+	int temp[n];
 	int i;
 
 	for (i = 0; i < n; i++)
-		aux[n - 1 - i] = a[i];
+		temp[n - 1 - i] = a[i];
 
 	for (i = 0; i < n; i++)
-		a[i] = aux[i];
+		a[i] = temp[i];
 }
