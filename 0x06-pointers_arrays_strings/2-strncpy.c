655d6fef862982ba654b74e59683d4c68c96e24d
Emarubu <emarubu945@gmail.com>
2022-07-14 00:03:08 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string
+* @src: first string
+* @dest - secomd string
+* Return: a pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for ( i = 0; i < n && src[i] !='\0';  i++)
+	{
+		dest[i] = src[i];
+	}
+		return (dest);
+}
