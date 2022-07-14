484c70059c73a1e2efa5df55d58cd1c4c90842a4
Emarubu <emarubu945@gmail.com>
2022-07-14 00:13:22 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,16 +3,21 @@
 /**
 * _strncpy - copies a string
 * @src: first string
-* @dest - secomd string
+* @dest: secomd string
+* @n: third
 * Return: a pointer
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for ( i = 0; i < n && src[i] !='\0';  i++)
-	{
+	for (i = 0; i < n && src[i] != '\0';  i++)
 		dest[i] = src[i];
-	}
-		return (dest);
+
+	for (; i < n; i++)
+
+	dest[i] = '\0';
+
+	return (dest);
+
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
- int _putchar(char c);
-=======
 int _putchar(char c);
->>>>>>> 2c2d85e891ec400d9bc1f4c18e29a135e2c644fb
+int _putchar(char c)
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
