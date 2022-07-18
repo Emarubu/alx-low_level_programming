15204f5a9a2a32687f03a760ac18179fe10e3256
Emarubu <emarubu945@gmail.com>
2022-07-18 18:10:25 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strstr - locates a substring
+* @haystack: first string
+* @needle: second
+* Return: a pointer or a Null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int k;
+
+	while (*haystack != '\0')
+	{
+	if (*needle != '\0')
+	haystack[i] == needle[i];
+	return (needle);
+	}
+		i++;
+		k++;
+		return (NULL);
+}
+
+
+
