dd721dc1ab6a9207e94163b4a6bf751210d4180b
Emarubu <emarubu945@gmail.com>
2022-07-22 15:39:50 +0000

    Silence is argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
 * main - entry point
-* prints number of arguments
-* Return: 0
+* @argc: argument count 
+* @argv: argument vector
+* Return: function name
 */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf("argc = %d\n", argc);
+	printf("argc = %d\n", argc -1);
 	return (0);
 }
