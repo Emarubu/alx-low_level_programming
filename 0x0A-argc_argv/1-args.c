50f5f493aefd0694f7447324c2f19eac9b67adeb
Emarubu <emarubu945@gmail.com>
2022-07-22 15:32:56 +0000

    Silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints number of arguments
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("argc = %d\n", argc);
+	return (0);
+}
