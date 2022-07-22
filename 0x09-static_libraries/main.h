44f741128c10a44de081600ea21045bfb5085491
Emarubu <emarubu945@gmail.com>
2022-07-22 12:01:20 +0000

    A library is not a luxury

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,5 @@
 #ifndef FILE_MAIN
+
 #define FILE_MAIN
 
 int _putchar(char c);
@@ -21,3 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
