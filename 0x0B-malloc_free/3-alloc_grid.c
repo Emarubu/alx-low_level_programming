f26d6c2447e21cd8ece107579f149bfe7f5d66e0
Emarubu <emarubu945@gmail.com>
2022-07-26 19:27:17 +0000

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,34 +2,40 @@
 #include <stdlib.h>
 
 /**
-* alloc_grid - eturns a pointer to a 2 dimensional array of integers
-* @width: columns
-* @height: rows
-* Return: NUL if FAIL and pointer to 2 dim array on success
-*/
+ * alloc_grid - return a pointer to a 2 dim array
+ * of integers
+ * @width: integer
+ * @height: integer
+ *
+ * Return: FAIL ? NULL : pointer to mem loc
+ */
+
 int **alloc_grid(int width, int height)
 {
-	int **my_array, i, j;
+	int **array, i, j;
 
-	if (width <= 0 || height <= 0)
+	if (height < 1 || width < 1)
 		return (NULL);
 
-	my_array = malloc(sizeof(int *) * height);
-	if (my_array == NULL)
-	{
-	return (NULL);
-	}
+	array = malloc(sizeof(int *) * height);
+
+	if (!array)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		my_array[i] = malloc(sizeof(int) * width);
+		array[i] = malloc(sizeof(int) * width);
 
-		if (my_array[i] == NULL)
+		if (!array[i])
 		{
+			for (j = 0; j < height; j++)
+				free(array[j]);
+			free(array);
 			return (NULL);
 		}
-
 		for (j = 0; j < width; j++)
-			my_array[i][j] = 0;
+			array[i][j] = 0;
 	}
-	return (my_array);
+
+	return (array);
 }
