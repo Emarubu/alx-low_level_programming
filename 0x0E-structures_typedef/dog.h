7f8e573ed6239c4093b4fa4561e9677b19ed256c
Emarubu <emarubu945@gmail.com>
2022-08-01 12:35:49 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog
+/**
+* struct dog - description of the dog
+*@name: name of dog
+*@age: age of the dog
+*@owner: owner od the dog
+*/
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
+	/*struct: for dog*/
 } dog;
 #endif
 
