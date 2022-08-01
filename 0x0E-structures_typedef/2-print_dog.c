2ccee1395f47c950f0bf7f9ba03fadd679c82e53
Emarubu <emarubu945@gmail.com>
2022-08-01 15:51:56 +0000

    things won't be too bad

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog - description of a dog
+* @d: a further description of the dog
+* Return: nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	(d->name == NULL) ? printf("nil\n") : printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	(d->owner == NULL) ? printf("nil\n") : printf("Owner: %s\n", d->owner);
+}
+
+
