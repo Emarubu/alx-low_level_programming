1abf0608f1b4b7a2eea2ee4d97ade6857e924e01
Emarubu <emarubu945@gmail.com>
2022-08-11 14:35:17 +0000

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+*list_len - returns number of elements in a link
+*@h: linked list
+*Return: number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	h->next;
+	n++;
+
+	return (n);
+}
