0226957141f94ec5099153ce731bf095cbbfb894
Emarubu <emarubu945@gmail.com>
2022-08-16 15:39:45 +0000

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
 * free_listint2 - freed list
@@ -19,5 +18,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-		head = NULL;
 }
