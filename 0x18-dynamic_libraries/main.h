8f5262b06c1c2adb0e140be9c7f6fc3e276730f0
Emarubu <emarubu945@gmail.com>
2022-10-03 18:59:08 +0000

    Without libraries what have we? We have no past and no future

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,6 @@
-#define FILE_MAIN
 #ifndef FILE_MAIN
-#include <string.h>
+#define FILE_MAIN
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
